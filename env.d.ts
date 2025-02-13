ccc31a330825d7a07e1269d7ab0f61d8e8088ea9
Utku Demir <50655392+Demir-Utku@users.noreply.github.com>
2025-02-13 14:41:21 +0300

    chore: add sentry config env variables

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SENTRY_DSN: string
+  readonly VITE_SENTRY_ORG: string
+  readonly VITE_SENTRY_PROJECT: string
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,42 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import Icons from 'unplugin-icons/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-    sentryVitePlugin({
-      org: 'utku-demir-fc',
-      project: 'horse-racing'
-    }),
-    Icons({
-      compiler: 'vue3'
-    })
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+      Icons({
+        compiler: 'vue3'
+      }),
+      sentryVitePlugin({
+        org: env.VITE_SENTRY_ORG,
+        project: env.VITE_SENTRY_PROJECT,
+        authToken: env.SENTRY_AUTH_TOKEN,
+        bundleSizeOptimizations: {
+          excludeDebugStatements: true
+        }
+      })
+    ],
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+
+    build: {
+      sourcemap: true
     }
-  },
-
-  build: {
-    sourcemap: true
   }
 })
