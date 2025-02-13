39c43d45b37dfa4bd8301b1cdc0f6093d8e506a1
Utku Demir <50655392+Demir-Utku@users.noreply.github.com>
2025-02-13 21:29:01 +0300

    feat: build App.vue component

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { onBeforeMount } from 'vue'
+
+import { useHorseStore } from '@/stores/horse'
+
+import AppHeader from './components/AppHeader.vue'
+import HorseList from './components/Race/HorseList.vue'
+import TrackArea from './components/Race/TrackArea.vue'
+import Results from './components/Race/Results.vue'
+import Program from './components/Race/Program.vue'
+
+const horseStore = useHorseStore()
+
+onBeforeMount(() => {
+  if (!horseStore.horses.length) {
+    horseStore.generateHorses()
+  }
+})
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <AppHeader />
+
+    <main class="flex-1 px-4 pb-4 pt-10 flex flex-col xl:flex-row xl:flex-wrap gap-6">
+      <section class="flex flex-col flex-1 md:flex-row gap-6">
+        <HorseList class="min-w-[14rem] md:max-w-3xs" />
+
+        <TrackArea class="flex-1" />
+      </section>
+
+      <section class="w-full xl:w-auto flex flex-col sm:flex-row gap-x-2 gap-y-6">
+        <Program class="min-w-[12rem] w-full sm:w-1/2 xl:max-w-[14rem]" />
+
+        <Results class="min-w-[12rem] w-full sm:w-1/2 xl:max-w-[14rem]" />
+      </section>
+    </main>
+  </div>
+</template>
